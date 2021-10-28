af4c8d355df653c07d227f546571d63da83b8a63
OIU085 <agoulart@bportugal.pt>
2021-10-28 19:13:19 +0100

    change mysql connection parms

--- nodejs-rest-api/build/app/config/db.config.js
+++ nodejs-rest-api/build/app/config/db.config.js
@@ -1,7 +1,7 @@
   // Database config 
   module.exports = {
     DB_HOST: (process.env.MYSQL_REST_SVC_SERVICE_HOST || 'localhost'),
-    DB_USER_NAME: (process.env.MYSQL_USER || 'restappu'),
-    DB_PASSWORD: (process.env.MYSQL_PASSWORD || 'mypa55'),
-    DB_NAME: (process.env.MYSQL_DATABASE || 'nodeApi_DB')
+    DB_USER_NAME: (process.env.MYSQL_REST_USER || 'restappu'),
+    DB_PASSWORD: (process.env.MYSQL_REST_PASSWORD || 'mypa55'),
+    DB_NAME: (process.env.MYSQL_REST_DATABASE || 'nodeapi_db')
   };
